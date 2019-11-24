4957e95c11dc22cf31496fbcfc63f246bf81be2a
Hayley Budzinski <hayleybudzinski@gmail.com>
2019-11-24 13:05:59 -0700

    got buttons working

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-var saveButton = $(".btn");
-var userInput = [];//whatever the user types in
+// var userInput = '';//whatever the user types in
 var currentDay = $("#currentDay");
+var input1 = document.getElementById("input1");
+var input2 = document.getElementById("input2");
 
-// var date = $("#date");
-// $("#current-day").append(date);
+document.getElementById("saveBtn").addEventListener("click", function(){
+    console.log("click");
+    var user = {
+        Task1: input1.value.trim(),
+        Task2: input2.value.trim()
+    };
+    // localStorage.setItem('input', []);//whatever the use types in
+    localStorage.setItem('Task 1:', user);
+    localStorage.setItem('Task 2:', user);
+    console.log(user);
+});
 
-// currentDay.moment().format('MMMM Do YYYY, h:mm:ss a');
-
-startButton.addEventListener("click", function(event){
-    localStorage.setItem('input', []);//whatever the use types in
-})
+//if current time block is in the past, assign class of .past 
+//if current time block is present, assign class of .present
+//if current time block is in the future, assign class of .future 
