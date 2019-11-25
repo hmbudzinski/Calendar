ca9cb0caf2b1c6a566bcae72675a13dfe8801696
Hayley Budzinski <hayleybudzinski@gmail.com>
2019-11-24 19:21:44 -0700

    set local storage

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 // var userInput = '';//whatever the user types in
 var currentDay = $("#currentDay");
-var input1 = document.getElementById("input1");
-var input2 = document.getElementById("input2");
+var text1 = document.getElementById("myText1");
+var text2 = document.getElementById("myText2");
 
-document.getElementById("saveBtn").addEventListener("click", function(){
+//8 am
+$("#saveBtn1").on("click", function(e){
+    e.preventDefault();
     console.log("click");
-    var user = {
-        Task1: input1.value.trim(),
-        Task2: input2.value.trim()
-    };
-    // localStorage.setItem('input', []);//whatever the use types in
-    localStorage.setItem('Task 1:', user);
-    localStorage.setItem('Task 2:', user);
-    console.log(user);
+    console.log($("#myText1").val());
+    localStorage.setItem('Task 1:', $("#myText1").val());
 });
+
+var local1 = window.localStorage.getItem('Task 1:');
+console.log(local1);
+text1.append(local1);
+
+//9am
+$("#saveBtn2").on("click", function(e){
+    e.preventDefault();
+    console.log("click2");
+    console.log($("#myText2").val());
+    localStorage.setItem('Task 2:', $("#myText2").val());
+});
+
+var local2 = window.localStorage.getItem('Task 2:');
+console.log(local2);
+text2.append(local2);
 
 //if current time block is in the past, assign class of .past 
 //if current time block is present, assign class of .present
